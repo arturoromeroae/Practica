4bc5ed5ac67decac90667ca88c841be63fb6e6d0
sandra flores <aromero@aeonlinesolutions.com>
2020-09-28 16:50:58 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registro</title>
+</head>
+<body>
+    <form action="POST">
+        <h1>Registro</h1>
+        <input type="text" name="name" placeholder:"nombre completo">
+        <input type="email" name="email" placeholder:"email">
+        <input type="submit" name="register">
+        <label for=""></label>
+    </form>
+
+        <?php
+        include("registrar.php");
+        ?>
+
+</body>
+</html>
