59d666f52ec7f834bdad2a61e932feadf81f1227
sandra flores <aromero@aeonlinesolutions.com>
2020-09-28 17:16:48 -0500

    se corrigen mas errores y se agrega estilo css

--- registrar.php
+++ registrar.php
@@ -3,26 +3,26 @@
 include("con_db.php");
 
 if (isset($_POST['register'])) {
-    if (strlen($_POST['name']) >= 1 && strlen($_POST["email"]) >= 1) {
+    if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
         $name = trim($_POST['name']);
         $email = trim($_POST['email']);
         $fechareg = date("d/m/y");
-        $consulta = "INSERT INTO datos(nombre, email, fecha_reg) VALUES ('$name','$email,'$fechareg')";
+        $consulta = "INSERT INTO datos(nombre, email, fecha_reg) VALUES ('$name','$email','$fechareg')";
         $resultado = mysqli_query($conex,$consulta);
 
         if ($resultado) {
             ?>
             <h3 class="ok">Se registro tu informacion</h3>
-            <?php
+        <?php
         } else {
             ?>
             <h3 class="bad">Hubo un error</h3>
-            <?php
+        <?php
         }
     } else {
         ?>
         <h3 class="bad">Por favor completa los campos</h3>
-        <?php
+    <?php
     }
 }
     
